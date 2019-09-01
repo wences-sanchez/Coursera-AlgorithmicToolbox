92ff90cfaa15bfce9d2bcc73a271820a1a75c737
Wenceslao Sanchez Pino <wenceslaosanchezpino@gmail.com>
2019-09-01 12:25:02 +0200

    Se han añadido nuevos test. Se ha refactorizado la llamada a tests anteriores.

--- test/wencesanchez/week3/CambioMonedaMinimoTest.java
+++ test/wencesanchez/week3/CambioMonedaMinimoTest.java
@@ -4,29 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CambioMonedaMinimoTest {
+public class CambioMonedaMinimoTest {
 
     @Test
     public void testDadoUnaMonedaDevuelveUno() {
         CambioMonedaMinimo cambioMonedaMinimo = new CambioMonedaMinimo();
-        long cambioDeUno = cambioMonedaMinimo.devolverCambio(1);
-        assertEquals(1, cambioDeUno, "cambioDeUno");
 
-        long cambioDeCinco = cambioMonedaMinimo.devolverCambio(5);
-        assertEquals(1, cambioDeCinco, "cambioDeCinco");
-
-        long cambioDeDiez = cambioMonedaMinimo.devolverCambio(10);
-        assertEquals(1, cambioDeDiez, "cambioDeDiez");
+        assertEquals(1, cambioMonedaMinimo.devolverCambio(1), "cambio de 1");
+        assertEquals(1, cambioMonedaMinimo.devolverCambio(5), "cambio de 5");
+        assertEquals(1, cambioMonedaMinimo.devolverCambio(10), "cambio de 10");
     }
 
     @Test
     public void testDadoElDobleDevuelveDosMonedas() {
         CambioMonedaMinimo cambioMonedaMinimo = new CambioMonedaMinimo();
-        long cambioDeDosDeUno = cambioMonedaMinimo.devolverCambio(2);
-        assertEquals(2, cambioDeDosDeUno, "cambioDeDosDeUno");
 
-        long cambioDeDosDeDiez = cambioMonedaMinimo.devolverCambio(20);
-        assertEquals(2, cambioDeDosDeDiez, "cambioDeDosDeDiez");
+        assertEquals(2, cambioMonedaMinimo.devolverCambio(2), "cambio de dos de 1");
+        assertEquals(2, cambioMonedaMinimo.devolverCambio(20), "cambio de dos de 10");
+        assertEquals(2, cambioMonedaMinimo.devolverCambio(6), "cambio de 5 y 1");
+        assertEquals(2, cambioMonedaMinimo.devolverCambio(11), "cambio de 10 y 1");
     }
 
     @Test
@@ -35,5 +31,8 @@ class CambioMonedaMinimoTest {
         long cambioDe8 = cambioMonedaMinimo.devolverCambio(8);
 
         assertEquals(4, cambioDe8, "cambio de 8");
+        assertEquals(5, cambioMonedaMinimo.devolverCambio(9), "cambio de 9");
+        assertEquals(4, cambioMonedaMinimo.devolverCambio(17), "cambio de 17");
+        assertEquals(3, cambioMonedaMinimo.devolverCambio(25), "cambio de 10, 10 y 5");
     }
 }
